2b1e031e05a9d103589d5bad243ed30df0a1edeb
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-31 13:50:53 +0100

    feat: add Picture image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 
 export default defineConfig({
   base: '/tests/test-astro',
   integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
     tailwind({
       config: {
         applyBaseStyles: false,
