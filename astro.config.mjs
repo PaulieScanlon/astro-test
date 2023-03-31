27c062b4e5cee20dac9b7476bc064c207f64752a
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-31 12:29:42 +0100

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   base: '/tests/test-astro',
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
 });
